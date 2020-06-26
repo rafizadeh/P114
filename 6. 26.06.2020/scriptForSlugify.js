8d7599aa5252d0d59cbb0d7c5b90fd20bb411d1f
rafizadeh <42311158+rafizadeh@users.noreply.github.com>
2020-06-26 19:51:10 +0400

    slug

---
+++ 6. 26.06.2020/scriptForSlugify.js
@@ -0,0 +1,28 @@
+﻿       String.prototype.removeAcento = function () {
+            var text = this.toLowerCase();
+            text = text.replace(new RegExp('[ÁÀÂÃ]', 'gi'), 'a');
+            text = text.replace(new RegExp('[ÉÈÊ]', 'gi'), 'e');
+            text = text.replace(new RegExp('[ÍÌÎ]', 'gi'), 'i');
+            text = text.replace(new RegExp('[ÓÒÔÕ]', 'gi'), 'o');
+            text = text.replace(new RegExp('[ÚÙÛÜ]', 'gi'), 'u');
+            text = text.replace(new RegExp('[Ç]', 'gi'), 'c');
+            text = text.replace(new RegExp('[Ə]', 'gi'), 'e');
+            text = text.replace(new RegExp('[I]', 'gi'), 'i');
+            text = text.replace(new RegExp('[ı]', 'gi'), 'i');
+            text = text.replace(new RegExp('[Ğ]', 'gi'), 'g');
+            text = text.replace(new RegExp('[ğ]', 'gi'), 'g');
+            return text;
+        };
+
+        String.prototype.slugify = function () {
+            return this.toString().toLowerCase().removeAcento().trim()
+                .replace(/\s+/g, '-')           // Replace spaces with -
+                .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+                .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+                .replace(/^-+/, '')             // Trim - from start of text
+                .replace(/-+$/, '');            // Trim - from end of text
+        };
+
+        $("input[data-type='slug-source']").blur(function () {
+            $("input[data-type='slug-input']").val($(this).val().slugify());
+        });
